3b780a517c19c6286a500156ebec5547895486c6
danyaman <dan.aman@yandex.ru>
2020-03-02 17:35:30 +0300

    task 3

--- src/main/java/com/efimchick/ifmo/web/jdbc/SetMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/SetMapperFactory.java
@@ -1,12 +1,62 @@
 package com.efimchick.ifmo.web.jdbc;
 
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
 
 public class SetMapperFactory {
 
     public SetMapper<Set<Employee>> employeesSetMapper() {
-        throw new UnsupportedOperationException();
+        return result -> {
+            Set<Employee> employeeSet = new HashSet<>();
+            try {
+                while (result.next()) {
+                    employeeSet.add(getEmployee(result));
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+            return employeeSet;
+        };
+    }
+
+    private Employee getEmployee(ResultSet result) throws SQLException {
+        try {
+            return new Employee(
+                    new BigInteger(result.getString("ID")),
+                    new FullName(
+                            result.getString("FIRSTNAME"),
+                            result.getString("LASTNAME"),
+                            result.getString("MIDDLENAME")),
+                    Position.valueOf(result.getString("POSITION")),
+                    LocalDate.parse(result.getString("HIREDATE")),
+                    result.getBigDecimal("SALARY"),
+                    getManager(result, result.getInt("MANAGER"))
+            );
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Employee getManager(ResultSet result, int managerId) throws SQLException {
+        Employee manager = null;
+        int rowNumber = result.getRow();
+        result.beforeFirst();
+
+        while (result.next()) {
+            if (result.getInt("ID") == managerId) {
+                manager = getEmployee(result);
+                break;
+            }
+        }
+        result.absolute(rowNumber);
+        return manager;
     }
 }
